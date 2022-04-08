9534c55056a1ff06aa14acdf6f9fd256cf05fe72
Joe Polny <joepolny@gmail.com>
2022-04-08 03:43:32 +0000

    better compile

--- teal.rb
+++ teal.rb
@@ -49,76 +49,46 @@ module TEALrb
   end
 
   class Compiler
-    attr_reader :teal
-    attr_accessor :if_count
+    attr_reader :teal, :vars
 
-    def initialize
+    def initialize(vars)
+      @vars = vars
       @teal = []
       @if_count = 0
+      @if_level = 0
     end
 
-    def compile(vars = {}, &blk)
-      seq = StringIO.new
-      seq.puts '['
+    @@eval_location = "#{__FILE__}:#{__LINE__ + 2}"
+    def teal_eval(str, vars = {})
+      eval("(#{str}).teal")
+    end
 
-      ifs = [nil]
-      if_level = 0
-
+    def compile(&blk)
       blk.source.lines[1..-2].each do |line|
         line.strip!
         next if line.empty?
 
         if line[/^if /]
-          if_level += 1
-          ifs[if_level] = { condition: line[/(?<=if ).*/].strip, seq: StringIO.new }
-          ifs[if_level][:seq].puts '['
-        elsif line[/^end/]
-          ifs[if_level][:seq].print ']'
-          seq.puts "self.if(#{ifs[if_level][:condition]}) do\n#{ifs[if_level][:seq].string}\nend,"
-          if_level -= 1
-        elsif ifs[if_level]
-          ifs[if_level][:seq].puts line + ','
+          new_if(line[/(?<=if ).*/])
+        elsif line == 'else'
+          @teal = @teal.map! { |t| t == "bnz if#{@if_level}_end" ? "bz if#{@if_level}_else" : t }
+          @teal += ["b if#{@if_level}_end"]
+          @teal += ["if#{@if_level}_else:"]
+        elsif line == 'end'
+          @teal += ["if#{@if_level}_end:"]
+          @if_level -= 1
         else
-          seq.puts line + ','
+            @teal += teal_eval(line, vars)
         end
       end
 
-      seq.print ']'
-
-      @teal = eval(seq.string).map(&:teal).flatten.compact
       self
     end
-    @@compile_location = "#{__FILE__}:#{__LINE__ - 3}"
 
-    @@sequence_location = "#{__FILE__}:#{__LINE__ + 2}"
-    def sequence(&blk)
-      @teal += blk.call.map(&:teal).flatten.compact
-    end
-
-    def if(conditional_expression, &blk)
-      IfBlock.new self, conditional_expression, blk
-    end
-  end
-
-  class IfBlock < Expression
-    def initialize(compiler, conditional, blk)
-      @id = compiler.if_count = compiler.if_count.og_add 1
-
-      @teal = [conditional.teal].flatten
-      @teal += ["bnz if#{@id}_end"]
-      @teal += blk.call.map(&:teal).flatten.compact
-      @teal += ["if#{@id}_end:"]
-    end
-
-    def else(&blk)
-      @else = true
-      @teal.pop
-      @teal = @teal.map! { |t| t == "bnz if#{@id}_end" ? "bnz if#{@id}_else" : t }
-      @teal += ["b if#{@id}_end"]
-      @teal += ["if#{@id}_else:"]
-      @teal += blk.call.map(&:teal).flatten.compact
-      @teal += ["if#{@id}_end:"]
-      self
+    def new_if(conditional)
+      @if_level +- 1
+      @teal += teal_eval(conditional, vars)
+      @teal += ["bnz if#{@if_level}_end"]
     end
   end
 end
@@ -134,10 +104,10 @@ class Integer
   alias og_more >
 
   def +(other)
-    from_seq = caller[-2]&.include? TEALrb::Compiler.class_variable_get :@@sequence_location
-    from_compile = caller[-2]&.include? TEALrb::Compiler.class_variable_get :@@compile_location
+    from_eval = caller.join.include? TEALrb::Compiler.class_variable_get :@@eval_location
+    from_pry = caller.join.include? 'pry'
 
-    if (from_seq or from_compile) and !caller.join.include? 'pry'
+    if from_eval and !from_pry
       TEALrb::Add.new self, other
     else
       og_add(other)
@@ -150,46 +120,6 @@ class NilClass
 end
 
 include TEALrb
-approval = Compiler.new
-a = approval # short name for if statements
-
-some_expression = app_global_get('howdy') + app_global_get('efefefe')
-
-foo = -> { 5 + 6 }
-bar = Int.new(7) + 8
-foobar = add(9, 10)
-
-approval.sequence do
-  [
-    1,
-    2,
-    add,
-    3 + 4,
-    foo.call,
-    bar,
-    foobar,
-    app_global_get
-  ]
-end
-
-a2 = Compiler.new
-
-a2.sequence do
-  [
-    a.if(1 + 2) do
-      [
-        3 + 4
-      ]
-    end.else do
-      [
-        5 + 6
-      ]
-    end,
-    5 + 6
-  ]
-end
-
-c = Compiler.new
 
 vars = {
   foo: -> { 5 + 6 },
@@ -197,16 +127,15 @@ vars = {
   foobar: add(9, 10),
 }
 
-c.compile(vars) do
-  1 + 2
-  app_global_get("some_key")
-  
-  if 3 + 4
-      vars[:foo].call
+c = Compiler.new(vars)
+
+c.compile do
+  if 1+2
+    3+4
+  else
+    5+6
   end
-  
-  vars[:bar]
-  vars[:foobar]
+  7+app_global_get('eight')
 end
 
 puts c.teal
