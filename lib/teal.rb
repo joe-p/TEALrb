f26192ec8e7c9a4d15bb03ef558ab29210ec249a
Joe Polny <joepolny@gmail.com>
2022-04-09 20:23:13 +0000

    breakout test

--- lib/teal.rb
+++ lib/teal.rb
@@ -164,30 +164,3 @@ end
 class NilClass
   def teal; end
 end
-
-include TEALrb
-
-approval = Compiler.new
-
-approval.defsub('increment_global') do |global_key, amount|
-  app_global_put(global_key, app_global_get(global_key) + amount)
-end
-
-approval.vars.foo = -> { 1 + 2 }
-approval.vars.bar = add(3, 4)
-
-approval.compile do
-  if vars.foo.call
-    vars.bar
-  elsif 5 + 6
-    7
-    8
-    add
-  else
-    add 9, 10
-  end
-
-  13 + 14 if app_global_get 'some_key'
-end
-
-puts approval.teal
