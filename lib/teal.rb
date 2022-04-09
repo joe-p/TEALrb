6130e4cd1ba0a20a75974baa0052713c3ccf784c
Joe Polny <joepolny@gmail.com>
2022-04-09 20:38:26 +0000

    use prepend

--- lib/teal.rb
+++ lib/teal.rb
@@ -2,13 +2,13 @@ require 'pry'
 require 'method_source'
 require_relative 'expressions'
 
-class String
-  def teal
-    TEALrb::Bytes.new(self).teal
-  end
-end
-
 module TEALrb
+  module StringMethods
+    def teal
+      TEALrb::Bytes.new(self).teal
+    end
+  end
+
   class If
     attr_accessor :blocks
     attr_reader :id
@@ -124,52 +124,44 @@ module TEALrb
 
     end
   end
-end
 
-class Integer
-  def teal
-    TEALrb::Int.new(self).teal
-  end
-
-  alias original_add +
-  alias original_subtract -
-  alias original_less <
-  alias original_greater >
-  alias original_less_eq <=
-  alias original_greater_eq >=
-
-  ORIGINAL_METHODS = {
-    '+': :original_add,
-    '-': :original_subtract,
-    '<': :original_less,
-    '>': :original_greater,
-    '<=': :original_less_eq,
-    '>=': :original_greater_eq,
-    '/': :original_divide
-  }
-
-  TEAL_CLASSES = {
-    '+': TEALrb::Add,
-    '-': TEALrb::Subtract,
-    '<': TEALrb::LessThan,
-    '>': TEALrb::GreaterThan,
-    '<=': TEALrb::LessThanOrEqual,
-    '>=': TEALrb::GreaterThanOrEqual,
-    '/': TEALrb::Divide
-  }
-
-  ORIGINAL_METHODS.keys.each do |meth|
-    define_method(meth) do |other|
-      from_eval = caller.join.include? TEALrb::Compiler.class_variable_get :@@eval_location
-      from_pry = caller.join.include? 'pry'
-
-      if from_eval and !from_pry
-        TEAL_CLASSES[meth].new self, other
-      else
-        self.send ORIGINAL_METHODS[meth], other
+  module IntegerMethods
+    def teal
+      TEALrb::Int.new(self).teal
+    end
+  
+    TEAL_CLASSES = {
+      '+': TEALrb::Add,
+      '-': TEALrb::Subtract,
+      '<': TEALrb::LessThan,
+      '>': TEALrb::GreaterThan,
+      '<=': TEALrb::LessThanOrEqual,
+      '>=': TEALrb::GreaterThanOrEqual,
+      '/': TEALrb::Divide
+    }
+  
+    TEAL_CLASSES.each do |meth, klass|
+      define_method(meth) do |other|
+        from_eval = caller.join.include? TEALrb::Compiler.class_variable_get :@@eval_location
+        from_pry = caller.join.include? 'pry'
+  
+        if from_eval and !from_pry
+          klass.new self, other
+        else
+          super(other)
+        end
       end
     end
   end
+end
+
+class Integer
+  prepend TEALrb::IntegerMethods
+end
+
+
+class String
+  prepend TEALrb::StringMethods
 end
 
 class NilClass
