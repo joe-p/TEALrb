78389d6b4e5495cb8f49298cae30165f05cdf530
Joe Polny <joepolny@gmail.com>
2022-04-12 22:15:33 +0000

    move math.rb to binary.rb

--- lib/expressions.rb
+++ lib/expressions.rb
@@ -1,6 +1,6 @@
 require_relative 'expressions/expression'
 require_relative 'expressions/crypto'
 require_relative 'expressions/flow'
-require_relative 'expressions/math'
+require_relative 'expressions/binary'
 require_relative 'expressions/storage'
 require_relative 'expressions/app'
