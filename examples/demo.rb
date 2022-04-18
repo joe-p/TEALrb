6f7cd45691d07b8b34b4eb1dde1f08d1ac1f7884
Joe Polny <joepolny@gmail.com>
2022-04-18 01:53:01 +0000

    rubocop

--- examples/demo.rb
+++ examples/demo.rb
@@ -7,8 +7,8 @@ class Approval < TEALrb::Contract
   # Specify ABI arg types, return type, and desc
   abi(
     args: {
-      a: {type: uint64, desc: 'The first number'},
-      b: {type: uint64, desc: 'The second number'}
+      a: { type: uint64, desc: 'The first number' },
+      b: { type: uint64, desc: 'The second number' }
     },
     returns: uint64,
     desc: 'Adds two numbers, subtracts two numbers, then multiplies two numbers'
